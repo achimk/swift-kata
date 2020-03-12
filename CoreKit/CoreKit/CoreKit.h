b0cd4f08f01bdcd29d5e2e0cb2ece48291b7231b
Joachim Kret <joachimkret@macbook.pro>
2020-03-12 20:02:31 +0100

    Added core kit framework.

---
+++ CoreKit/CoreKit/CoreKit.h
@@ -0,0 +1,19 @@
+//
+//  CoreKit.h
+//  CoreKit
+//
+//  Created by Joachim Kret on 12/03/2020.
+//  Copyright © 2020 Joachim Kret. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreKit.
+FOUNDATION_EXPORT double CoreKitVersionNumber;
+
+//! Project version string for CoreKit.
+FOUNDATION_EXPORT const unsigned char CoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreKit/PublicHeader.h>
+
+
