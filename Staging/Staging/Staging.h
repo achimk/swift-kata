8616cd9c6cf3682befe8b58dc16aada1ffc2fbcb
Joachim Kret <joachimkret@macbook.pro>
2020-03-12 20:01:29 +0100

    Added staging framework.

---
+++ Staging/Staging/Staging.h
@@ -0,0 +1,19 @@
+//
+//  Staging.h
+//  Staging
+//
+//  Created by Joachim Kret on 12/03/2020.
+//  Copyright © 2020 Joachim Kret. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Staging.
+FOUNDATION_EXPORT double StagingVersionNumber;
+
+//! Project version string for Staging.
+FOUNDATION_EXPORT const unsigned char StagingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Staging/PublicHeader.h>
+
+
