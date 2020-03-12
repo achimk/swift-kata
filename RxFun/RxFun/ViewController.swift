bb6ea26c1af57d0b575c42891dd6396cdf8cd067
Joachim Kret <joachimkret@macbook.pro>
2020-03-12 20:07:01 +0100

    Added rx func sample app.

---
+++ RxFun/RxFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxFun
+//
+//  Created by Joachim Kret on 12/03/2020.
+//  Copyright © 2020 Joachim Kret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
