e504e66c3ec200507002ca14d4f0502d14f10690
Joachim Kret <joachimkret@macbook.pro>
2020-06-08 19:48:58 +0200

    Added linked list.

---
+++ CoreKit/CoreKit/Algorithms/LinkedList.swift
@@ -0,0 +1,340 @@
+//
+//  Created by Joachim Kret on 08/06/2020.
+//  Copyright © 2020 Joachim Kret. All rights reserved.
+//
+
+import Foundation
+
+public enum LinkedListError: Error {
+    case empty
+    case outOfBounds
+}
+
+// MARK: - LinkedList
+
+public struct LinkedList<T> {
+    
+    typealias Node = LinkedListStorage<T>.Node
+    private var storage: LinkedListStorage<T>
+    
+    var head: Node? { return storage.head }
+    var tail: Node? { return storage.tail }
+    
+    fileprivate init(_ storage: LinkedListStorage<T>) {
+        self.storage = storage
+    }
+    
+    public init() {
+        storage = LinkedListStorage()
+    }
+    
+    public init(_ element: T) {
+        storage = LinkedListStorage(element)
+    }
+    
+    public init(_ elements: T...) {
+        storage = LinkedListStorage(elements)
+    }
+    
+    public init(_ elements: [T]) {
+        storage = LinkedListStorage(elements)
+    }
+}
+
+// MARK: - LinkedList (Operations)
+
+extension LinkedList {
+    
+    public mutating func append(_ element: T) {
+        makeUnique()
+        storage.append(element)
+    }
+    
+    public mutating func insert(_ element: T, at index: Int) {
+        makeUnique()
+        storage.insert(element, at: index)
+    }
+    
+    public mutating func update(_ element: T, at index: Int) {
+        makeUnique()
+        storage.update(element, at: index)
+    }
+    
+    public mutating func remove(at index: Int) {
+        makeUnique()
+        storage.remove(at: index)
+    }
+    
+    public mutating func removeAll() {
+        makeUnique()
+        storage.removeAll()
+    }
+}
+
+// MARK: - LinkedList (Collection)
+
+extension LinkedList: Collection {
+    
+    public var count: Int {
+        return storage.count
+    }
+    
+    public var startIndex: Int {
+        return storage.startIndex
+    }
+    
+    public var endIndex: Int {
+        return storage.endIndex
+    }
+    
+    public func index(after i: Int) -> Int {
+        return storage.index(after: i)
+    }
+    
+    public subscript(position: Int) -> T {
+        return storage[position]
+    }
+    
+    public subscript(safe position: Int) -> T? {
+        if storage.indices.contains(position) {
+            return storage[position]
+        } else {
+            return nil
+        }
+    }
+}
+
+extension LinkedList: RandomAccessCollection { }
+
+// MARK: - LinkedList (Iterators)
+
+extension LinkedList {
+    
+    public func makeForewardIterator() -> ForewardLinkedListIterator<T> {
+        return ForewardLinkedListIterator(self)
+    }
+    
+    public func makeBackwardIterator() -> BackwardLinkedListIterator<T> {
+        return BackwardLinkedListIterator(self)
+    }
+}
+
+extension LinkedList {
+
+    private mutating func makeUnique() {
+        if !isKnownUniquelyReferenced(&storage) {
+            storage = storage.cloned()
+        }
+    }
+}
+
+// MARK: - ForewardLinkedListIterator
+
+public struct ForewardLinkedListIterator<T>: IteratorProtocol, Sequence {
+    
+    private let list: LinkedList<T>
+    private var node: LinkedList<T>.Node?
+    
+    public init(_ list: LinkedList<T>) {
+        self.list = list
+        self.node = list.head
+    }
+    
+    public mutating func next() -> T? {
+        let value = node?.value
+        self.node = node?.next
+        return value
+    }
+}
+
+// MARK: - BackwardLinkedListIterator
+
+public struct BackwardLinkedListIterator<T>: IteratorProtocol, Sequence {
+    
+    private let list: LinkedList<T>
+    private var node: LinkedList<T>.Node?
+    
+    public init(_ list: LinkedList<T>) {
+        self.list = list
+        self.node = list.tail
+    }
+    
+    public mutating func next() -> T? {
+        let value = node?.value
+        self.node = node?.previous
+        return value
+    }
+}
+
+// MARK: - LinkedListStorage (Container)
+
+final class LinkedListStorage<T> {
+    
+    final class Node {
+        var value: T
+        var next: Node?
+        weak var previous: Node?
+        init(_ value: T) { self.value = value }
+    }
+    
+    private(set) var counter: Int
+    private(set) var head: Node?
+    private(set) var tail: Node?
+    
+    init() {
+        counter = 0
+        head = nil
+        tail = nil
+    }
+    
+    init(_ element: T) {
+        counter = 1
+        let node = Node(element)
+        head = node
+        tail = node
+    }
+    
+    init(_ elements: [T]) {
+        counter = elements.count
+        if let first = elements.first {
+            let node = Node(first)
+            head = node
+            tail = node
+            
+            let list = Array(elements.dropFirst())
+            list.forEach { (element) in
+                let node = Node(element)
+                tail?.next = node
+                node.previous = tail
+                tail = node
+            }
+        } else {
+            head = nil
+            tail = nil
+        }
+    }
+}
+
+extension LinkedListStorage {
+    
+    func append(_ element: T) {
+        counter = counter.advanced(by: 1)
+        let node = Node(element)
+        if tail == nil {
+            head = node
+            tail = node
+        } else {
+            tail?.next = node
+            node.previous = tail
+            tail = node
+        }
+    }
+    
+    func insert(_ element: T, at index: Int) {
+        try! _insert(element, at: index, node: head)
+    }
+    
+    private func _insert(_ element: T, at index: Int, node: Node?) throws {
+        switch index {
+        case 0:
+            counter = counter.advanced(by: 1)
+            let newNode = Node(element)
+            let previous = node == nil ? tail : node?.previous
+            let next = node
+            
+            newNode.previous = previous
+            newNode.next = next
+            next?.previous = newNode
+            previous?.next = newNode
+            
+            if head == nil { head = newNode }
+            else if head === next { head = newNode }
+            if tail == nil { tail = newNode }
+            else if tail === previous { tail = newNode }
+        default:
+            guard index > 0, index <= counter else { throw LinkedListError.outOfBounds }
+            try _insert(element, at: index - 1, node: node?.next)
+        }
+    }
+    
+    func update(_ element: T, at index: Int) {
+        _update(element, at: index, node: head)
+    }
+    
+    private func _update(_ element: T, at index: Int, node: Node?) {
+        switch index {
+        case 0:
+            node!.value = element
+        default:
+            _update(element, at: index - 1, node: node?.next)
+        }
+    }
+    
+    func remove(at index: Int) {
+        try! _remove(at: index, node: head)
+    }
+    
+    private func _remove(at index: Int, node: Node?) throws {
+        switch index {
+        case 0:
+            counter = counter.advanced(by: -1)
+            let previous = node?.previous
+            let next = node?.next
+            previous?.next = next
+            next?.previous = previous
+            if previous == nil { head = next }
+            if next == nil { tail = previous }
+        default:
+            guard index > 0, index < counter else { throw LinkedListError.outOfBounds }
+            try _remove(at: index - 1, node: node?.next)
+        }
+    }
+    
+    func removeAll() {
+        counter = 0
+        head = nil
+        tail = nil
+    }
+}
+
+extension LinkedListStorage: Collection {
+    
+    var count: Int {
+        return counter
+    }
+    
+    var startIndex: Int {
+        return 0
+    }
+    
+    var endIndex: Int {
+        return count
+    }
+    
+    func index(after i: Int) -> Int {
+        return i.advanced(by: 1)
+    }
+    
+    subscript(position: Int) -> T {
+        return try! _value(at: position, node: head)
+    }
+    
+    private func _value(at position: Int, node: Node?) throws -> T {
+        switch position {
+        case 0:
+            guard let node = node else {
+                throw LinkedListError.empty
+            }
+            return node.value
+        default:
+            return try _value(at: position - 1, node: node?.next)
+        }
+    }
+}
+
+extension LinkedListStorage {
+    
+    func cloned() -> LinkedListStorage {
+        return LinkedListStorage(Array(makeIterator()))
+    }
+}
---
+++ CoreKit/CoreKitTests/Algorithms/LinkedListTests.swift
@@ -0,0 +1,467 @@
+//
+//  Created by Joachim Kret on 08/06/2020.
+//  Copyright © 2020 Joachim Kret. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import CoreKit
+
+final class LinkedListTests: XCTestCase {
+    
+    // MARK: Test Create
+    
+    func test_createEmpty_shouldBeEmpty() {
+        let list = LinkedList<Int>()
+        expect(list.isEmpty) == true
+    }
+    
+    func test_createWithOneElement_shouldNotBeEmpty() {
+        let list = LinkedList<Int>(1)
+        expect(list.isEmpty) == false
+    }
+    
+    func test_createWithEmptyList_shouldBeEmpty() {
+        let list = LinkedList<Int>([])
+        expect(list.isEmpty) == true
+    }
+    
+    func test_createWithNonEmptyArguments_shouldNotBeEmpty() {
+        let list = LinkedList<Int>(1, 2, 3)
+        expect(list.isEmpty) == false
+    }
+    
+    func test_createWithNonEmptyList_shouldNotBeEmpty() {
+        let list = LinkedList<Int>([1, 2, 3])
+        expect(list.isEmpty) == false
+    }
+    
+    func test_createWithOneElement_shouldMatchElement() {
+        let list = LinkedList<Int>(1)
+        let it = Array(list.makeIterator())
+        expect(it) == [1]
+    }
+    
+    func test_createWithNonEmptyArguments_shouldMatchElements() {
+        let list = LinkedList<Int>(1, 2, 3)
+        let it = Array(list.makeIterator())
+        expect(it) == [1, 2, 3]
+    }
+    
+    func test_createWithNonEmptyList_shouldMatchElements() {
+        let list = LinkedList<Int>([1, 2, 3])
+        let it = Array(list.makeIterator())
+        expect(it) == [1, 2, 3]
+    }
+    
+    // MARK: Test Append
+    
+    func test_appendElement_shouldContainAppendedElement() {
+        var list = LinkedList<Int>()
+        list.append(1)
+        expect(Array(list.makeIterator())) == [1]
+    }
+    
+    func test_multipleAppends_shouldContainAllElements() {
+        var list = LinkedList<Int>()
+        list.append(1)
+        list.append(2)
+        list.append(3)
+        expect(Array(list.makeIterator())) == [1, 2, 3]
+    }
+    
+    // MARK: Test Insert
+    
+    func test_insertToEmptyList_shouldContainFirstElement() {
+        var list = LinkedList<Int>()
+        list.insert(1, at: 0)
+        expect(Array(list.makeIterator())) == [1]
+    }
+    
+    func test_insertFirstIndexToOneElementList_elementShouldBeFirst() {
+        var list = LinkedList<Int>(1)
+        list.insert(4, at: 0)
+        expect(Array(list.makeIterator())) == [4, 1]
+    }
+    
+    func test_insertLastIndexToOneElementList_elementShouldBeLast() {
+        var list = LinkedList<Int>(1)
+        list.insert(4, at: 1)
+        expect(Array(list.makeIterator())) == [1, 4]
+    }
+    
+    func test_insertFirstIndex_elementShouldBeFirst() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 0)
+        expect(Array(list.makeIterator())) == [4, 1, 2, 3]
+    }
+    
+    func test_insertLastIndex_elementShouldBeLast() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 3)
+        expect(list.count) == 4
+        expect(list.last) == 4
+        expect(Array(list.makeIterator())) == [1, 2, 3, 4]
+    }
+    
+    func test_insertMiddleIndex_elementShouldBeIntMiddleIndex() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 2)
+        expect(Array(list.makeIterator())) == [1, 2, 4, 3]
+    }
+    
+    func test_multipleInsertionToEmptyList_shouldMatch() {
+        var list = LinkedList<Int>()
+        list.insert(1, at: 0)
+        list.insert(2, at: 0)
+        list.insert(3, at: 2)
+        list.insert(4, at: 2)
+        list.insert(5, at: 4)
+        expect(Array(list.makeIterator())) == [2, 1, 4, 3, 5]
+    }
+    
+    func test_multipleInsertionToNonEmptyList_shouldMatch() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(1, at: 0)
+        list.insert(2, at: 0)
+        list.insert(3, at: 4)
+        list.insert(4, at: 4)
+        list.insert(5, at: 7)
+        expect(Array(list.makeIterator())) == [2, 1, 1, 2, 4, 3, 3, 5]
+    }
+    
+    // MARK: Test Update
+    
+    func test_updateFirstIndex_shouldBeModified() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.update(4, at: 0)
+        expect(Array(list.makeIterator())) == [4, 2, 3]
+    }
+    
+    func test_updateLastIndex_shouldBeModified() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.update(4, at: 2)
+        expect(Array(list.makeIterator())) == [1, 2, 4]
+    }
+    
+    func test_updateMiddleIndex_shouldBeModified() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.update(4, at: 1)
+        expect(Array(list.makeIterator())) == [1, 4, 3]
+    }
+    
+    func test_updateAll_shouldHaveAllModified() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.update(4, at: 0)
+        list.update(4, at: 1)
+        list.update(4, at: 2)
+        expect(Array(list.makeIterator())) == [4, 4, 4]
+    }
+    
+    // MARK: Test Remove
+    
+    func test_removeElementFromOneElementList_shouldBeEmpty() {
+        var list = LinkedList<Int>(1)
+        list.remove(at: 0)
+        expect(list.isEmpty) == true
+    }
+    
+    func test_removeFirstIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 0)
+        expect(Array(list.makeIterator())) == [2, 3]
+    }
+    
+    func test_removeLastIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 2)
+        expect(Array(list.makeIterator())) == [1, 2]
+    }
+    
+    func test_removeMiddleIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 1)
+        expect(Array(list.makeIterator())) == [1, 3]
+    }
+    
+    func test_removeAllElementsFromFront_shouldBeEmpty() {
+        var list = LinkedList<Int>(1, 2, 3)
+        var count = list.count
+        while count > 0 {
+            count -= 1
+            list.remove(at: 0)
+        }
+        expect(list.isEmpty) == true
+    }
+    
+    func test_removeAllElementsFromBack_shouldBeEmpty() {
+        var list = LinkedList<Int>(1, 2, 3)
+        var index = list.count - 1
+        repeat {
+            list.remove(at: index)
+            index -= 1
+        } while index >= 0
+        expect(list.isEmpty) == true
+    }
+    
+    // MARK: Test Remove All
+    
+    func test_removeAllOnEmptyList_shouldBeEmpty() {
+        var list = LinkedList<Int>()
+        list.removeAll()
+        expect(list.isEmpty) == true
+    }
+    
+    func test_removeAllOnNontEmptyList_shouldBeEmpty() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.removeAll()
+        expect(list.isEmpty) == true
+    }
+    
+    // MARK: Test Subscript
+    
+    func test_subscriptValues_shouldReturnValidValue() {
+        let list = LinkedList<Int>(1, 2, 3)
+        expect(list[0]) == 1
+        expect(list[1]) == 2
+        expect(list[2]) == 3
+    }
+    
+    func test_safeSubscriptValues_shouldReturnValidValue() {
+        let list = LinkedList<Int>(1, 2, 3)
+        expect(list[safe: 0]) == 1
+        expect(list[safe: 1]) == 2
+        expect(list[safe: 2]) == 3
+        expect(list[safe: 3]).to(beNil())
+    }
+    
+    // MARK: Test Count
+    
+    func test_emptyList_shouldBeZero() {
+        let list = LinkedList<Int>()
+        expect(list.count) == 0
+    }
+    
+    func test_nonEmptyList_shouldMatchCount() {
+        let list = LinkedList<Int>(1, 2, 3)
+        expect(list.count) == 3
+    }
+    
+    // MARK: Test Ierators
+    
+    func test_defaultInterator_shouldIterateAllValues() {
+        let list = LinkedList<Int>(1, 2, 3, 4, 5)
+        let it = Array(list.makeIterator())
+        expect(it) == [1, 2, 3, 4, 5]
+    }
+    
+    func test_forewardIterator_shouldIterateAllValuesWithRightOrder() {
+        let list = LinkedList<Int>(1, 2, 3, 4, 5)
+        let it = Array(list.makeForewardIterator())
+        expect(it) == [1, 2, 3, 4, 5]
+    }
+    
+    func test_backwardIterator_shouldIterateAllValuesWithRightOrder() {
+        let list = LinkedList<Int>(1, 2, 3, 4, 5)
+        let it = Array(list.makeBackwardIterator())
+        expect(it) == [5, 4, 3, 2, 1]
+    }
+}
+
+final class LinkedListForewardIteratorTests: XCTestCase {
+    
+    // MARK: Test Append
+    
+    func test_appendElement_shouldContainAppendedElement() {
+        var list = LinkedList<Int>()
+        list.append(1)
+        let it = Array(list.makeForewardIterator())
+        expect(it) == [1]
+    }
+    
+    func test_multipleAppends_shouldContainAllElements() {
+        var list = LinkedList<Int>()
+        list.append(1)
+        list.append(2)
+        list.append(3)
+        let it = Array(list.makeForewardIterator())
+        expect(it) == [1, 2, 3]
+    }
+    
+    // MARK: Test Insert
+    
+    func test_insertToEmptyList_shouldContainFirstElement() {
+        var list = LinkedList<Int>()
+        list.insert(1, at: 0)
+        expect(Array(list.makeForewardIterator())) == [1]
+    }
+    
+    func test_insertFirstIndexToOneElementList_elementShouldBeFirst() {
+        var list = LinkedList<Int>(1)
+        list.insert(4, at: 0)
+        expect(Array(list.makeForewardIterator())) == [4, 1]
+    }
+    
+    func test_insertLastIndexToOneElementList_elementShouldBeLast() {
+        var list = LinkedList<Int>(1)
+        list.insert(4, at: 1)
+        expect(Array(list.makeForewardIterator())) == [1, 4]
+    }
+    
+    func test_insertFirstIndex_elementShouldBeFirst() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 0)
+        expect(Array(list.makeForewardIterator())) == [4, 1, 2, 3]
+    }
+    
+    func test_insertLastIndex_elementShouldBeLast() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 3)
+        expect(Array(list.makeForewardIterator())) == [1, 2, 3, 4]
+    }
+    
+    func test_insertMiddleIndex_elementShouldBeIntMiddleIndex() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 2)
+        expect(Array(list.makeForewardIterator())) == [1, 2, 4, 3]
+    }
+    
+    func test_multipleInsertionToEmptyList_shouldMatch() {
+        var list = LinkedList<Int>()
+        list.insert(1, at: 0)
+        list.insert(2, at: 0)
+        list.insert(3, at: 2)
+        list.insert(4, at: 2)
+        list.insert(5, at: 4)
+        expect(Array(list.makeForewardIterator())) == [2, 1, 4, 3, 5]
+    }
+    
+    func test_multipleInsertionToNonEmptyList_shouldMatch() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(1, at: 0)
+        list.insert(2, at: 0)
+        list.insert(3, at: 4)
+        list.insert(4, at: 4)
+        list.insert(5, at: 7)
+        expect(Array(list.makeForewardIterator())) == [2, 1, 1, 2, 4, 3, 3, 5]
+    }
+    
+    // MARK: Test Remove
+    
+    func test_removeFirstIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 0)
+        expect(Array(list.makeForewardIterator())) == [2, 3]
+    }
+    
+    func test_removeLastIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 2)
+        expect(Array(list.makeForewardIterator())) == [1, 2]
+    }
+    
+    func test_removeMiddleIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 1)
+        expect(Array(list.makeForewardIterator())) == [1, 3]
+    }
+}
+
+final class LinkedListBackwardIteratorTests: XCTestCase {
+    
+    // MARK: Test Append
+    
+    func test_appendElement_shouldContainAppendedElement() {
+        var list = LinkedList<Int>()
+        list.append(1)
+        let it = Array(list.makeBackwardIterator())
+        expect(it) == [1]
+    }
+    
+    func test_multipleAppends_shouldContainAllElements() {
+        var list = LinkedList<Int>()
+        list.append(1)
+        list.append(2)
+        list.append(3)
+        let it = Array(list.makeBackwardIterator())
+        expect(it) == [3, 2, 1]
+    }
+    
+    // MARK: Test Insert
+    
+    func test_insertToEmptyList_shouldContainFirstElement() {
+        var list = LinkedList<Int>()
+        list.insert(1, at: 0)
+        expect(Array(list.makeBackwardIterator())) == [1]
+    }
+    
+    func test_insertFirstIndexToOneElementList_elementShouldBeFirst() {
+        var list = LinkedList<Int>(1)
+        list.insert(4, at: 0)
+        expect(Array(list.makeBackwardIterator())) == [1, 4]
+    }
+    
+    func test_insertLastIndexToOneElementList_elementShouldBeLast() {
+        var list = LinkedList<Int>(1)
+        list.insert(4, at: 1)
+        expect(Array(list.makeBackwardIterator())) == [4, 1]
+    }
+    
+    func test_insertFirstIndex_elementShouldBeFirst() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 0)
+        expect(Array(list.makeBackwardIterator())) == [3, 2, 1, 4]
+    }
+    
+    func test_insertLastIndex_elementShouldBeLast() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 3)
+        expect(Array(list.makeBackwardIterator())) == [4, 3, 2, 1]
+    }
+    
+    func test_insertMiddleIndex_elementShouldBeIntMiddleIndex() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(4, at: 2)
+        expect(Array(list.makeBackwardIterator())) == [3, 4, 2, 1]
+    }
+    
+    func test_multipleInsertionToEmptyList_shouldMatch() {
+        var list = LinkedList<Int>()
+        list.insert(1, at: 0)
+        list.insert(2, at: 0)
+        list.insert(3, at: 2)
+        list.insert(4, at: 2)
+        list.insert(5, at: 4)
+        expect(Array(list.makeBackwardIterator())) == [5, 3, 4, 1, 2]
+    }
+    
+    func test_multipleInsertionToNonEmptyList_shouldMatch() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.insert(1, at: 0)
+        list.insert(2, at: 0)
+        list.insert(3, at: 4)
+        list.insert(4, at: 4)
+        list.insert(5, at: 7)
+        expect(Array(list.makeBackwardIterator())) == [5, 3, 3, 4, 2, 1, 1, 2]
+    }
+    
+    // MARK: Test Remove
+    
+    func test_removeFirstIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 0)
+        expect(Array(list.makeBackwardIterator())) == [3, 2]
+    }
+    
+    func test_removeLastIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 2)
+        expect(Array(list.makeBackwardIterator())) == [2, 1]
+    }
+    
+    func test_removeMiddleIndex_shouldNotContainElement() {
+        var list = LinkedList<Int>(1, 2, 3)
+        list.remove(at: 1)
+        expect(Array(list.makeBackwardIterator())) == [3, 1]
+    }
+}
